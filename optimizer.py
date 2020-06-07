675ccc842e1a08eeef164e5c2263049ed9fedb6c
harshv834 <harshv834@gmail.com>
2020-06-07 02:23:50 +0200

    optimizers addded

--- optimizer.py
+++ optimizer.py
@@ -12,6 +12,7 @@ import os
 from collections import OrderedDict
 from torch.utils.data import Subset
 from torch.optim.optimizer import Optimizer
+from model_util import *
 
 class EFSGD(Optimizer):
     def __init__(self, params, lr ):
@@ -21,7 +22,6 @@ class EFSGD(Optimizer):
                 state = self.state[param]
                 state['error_correction'] = torch.zeros_like( param.data )
                 state['lr'] = lr
-                state['update'] = OrderedDict()
                 
                     
     def step(self):
@@ -38,15 +38,14 @@ class EFSGD(Optimizer):
                 #EFSGD
                 g = ( torch.sum( torch.abs(p) )/p.nelement() ) * torch.sign(p)
                 state['error_correction'] = p - g
-                state['update'][k] = g
+                state['update'] = g
 
-class QEFSGD(Optimizer):
+class signSGD(Optimizer):
     def __init__(self, params, lr ):
         super(EFSGD,self).__init__( params , dict( lr = lr ) )
         for group in self.param_groups:
             for param in group['params']:
                 state = self.state[param]
-                state['error_correction'] = torch.zeros_like( param.data )
                 state['lr'] = lr
                 
                     
@@ -56,12 +55,89 @@ class QEFSGD(Optimizer):
                 if param.grad is None:
                     continue
                 state = self.state[param] 
-                error_corr = state['error_correction']
                 lr = state['lr']
-                p = param.grad.data
-                p = lr*p + error_corr 
+                state['update'] = lr*param.grad.data.sign()
+
                 
-                #EFSGD
-                g = ( torch.sum( torch.abs(p) )/p.nelement() ) * torch.sign(p)
-                state['error_correction'] = p - g
-                param.data = param.data - g
+
+class QSGD_lossy(Optimizer):
+    def __init__(self, params, lr ):
+        super(EFSGD,self).__init__( params , dict( lr = lr ) )
+        for group in self.param_groups:
+            for param in group['params']:
+                state = self.state[param]
+                state['lr'] = lr
+                
+                    
+    def step(self):
+        for group in self.param_groups:
+            for k,param in enumerate(group['params']):
+                if param.grad is None:
+                    continue
+                state = self.state[param] 
+                lr = state['lr']
+                state['update'] = lr*quantizer_lossy(param.grad.data)
+
+
+class QSGD_topk(Optimizer):
+    def __init__(self, params, lr ):
+        super(EFSGD,self).__init__( params , dict( lr = lr ) )
+        for group in self.param_groups:
+            for param in group['params']:
+                state = self.state[param]
+                state['lr'] = lr
+                
+                    
+    def step(self):
+        for group in self.param_groups:
+            for k,param in enumerate(group['params']):
+                if param.grad is None:
+                    continue
+                state = self.state[param] 
+                lr = state['lr']
+                state['update'] = lr*quantizer_topk(param.grad.data)
+
+                
+class QEFSGD_lossy(Optimizer):
+    def __init__(self, params, lr,beta,alpha):
+        super(EFSGD,self).__init__( params , dict( lr = lr, beta = beta,alpha=alpha) )
+        for group in self.param_groups:
+            for param in group['params']:
+                state = self.state[param]
+                state['error_correction'] = torch.zeros_like( param.data )
+                state['lr'] = lr
+                state['beta'] =beta
+                state['alpha'] =alpha
+                    
+    def step(self):
+        for group in self.param_groups:
+            for k,param in enumerate(group['params']):
+                if param.grad is None:
+                    continue
+                state = self.state[param] 
+                lr = state['lr']
+                state['update'] = lr*quantizer_lossy(state['error_correction']*state['alpha'] + param.grad.data)
+                state['error_correction'] = beta*state['error_correction'] - state['update'] +param.grad.data 
+                
+
+class QEFSGD_topk(Optimizer):
+    def __init__(self, params, lr,beta,alpha):
+        super(EFSGD,self).__init__( params , dict( lr = lr, beta = beta,alpha=alpha) )
+        for group in self.param_groups:
+            for param in group['params']:
+                state = self.state[param]
+                state['error_correction'] = torch.zeros_like( param.data )
+                state['lr'] = lr
+                state['beta'] =beta
+                state['alpha'] =alpha
+                    
+    def step(self):
+        for group in self.param_groups:
+            for k,param in enumerate(group['params']):
+                if param.grad is None:
+                    continue
+                state = self.state[param] 
+                lr = state['lr']
+                state['update'] = lr*quantizer_topk(state['error_correction']*state['alpha'] + param.grad.data)
+                state['error_correction'] = beta*state['error_correction'] - state['update'] +param.grad.data 
+                
