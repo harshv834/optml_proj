a03cda2674f1bb54a9f03559203002d83b6479aa
harshv834 <harshv834@gmail.com>
2020-06-12 23:31:23 +0200

    additional comments removed

--- run.py
+++ run.py
@@ -39,12 +39,23 @@ num_workers = 9
 sqrt_num_workers = 3
 m = trainset_node_split(trainset, num_workers)
 trainloaders = [torch.utils.data.DataLoader(m[i], batch_size=32, shuffle=True, num_workers=2) for i in range(num_workers)]
+
+
 W = torus(sqrt_num_workers)
 lrs = []
+
+# uncommit here to run for the ring
+# W = ring(num_workers)
+# lrs = []
+
+
 
 # EFSGD, signSGD, QSGD_lossy, QEFSGD_lossy, QSGD_topk, QEFSGD_topk
 # can choose optimizer from one of the above.
 optimizer = EFSGD
+
+
+
 
 for i in range(num_workers):
 	# Uncomment this in case of QEFSGD_lossy
@@ -71,9 +82,9 @@ beta_protec = 2/5
 # beta_protec = 2/5 ( choose according to architecture )
 # Used when running the protect method using frac_mean
 
-net = Network(W, models, m, lrs, trainloaders, 32, nn.CrossEntropyLoss(), device, testloader, signSGD, [3,5], attack, protect, beta_protec  )
+net = Network(W, models, m, lrs, trainloaders, 32, nn.CrossEntropyLoss(), device, testloader, signSGD, [3,5], attack, protec, beta_protec  )
 
-iterations = 3000
+iterations = 100
 epochs = 1
 results = net.simulate(iterations, epochs)
 
@@ -96,53 +107,5 @@ plt.savefig("Consensus_train_accuracy_Torus.jpg")
 # Uncomment to dump a pickle file.
 # pickle.dump(a,open("ring_signsgd_fullr_2node.pickle","wb"))
 
-# uncommit here to run for the ring
-# W = ring(num_workers)
-# lrs = []
 
-# # EFSGD, signSGD, QSGD_lossy, QEFSGD_lossy, QSGD_topk, QEFSGD_topk
-# # can choose optimizer from one of the above.
-# optimizer = EFSGD
 
-# for i in range(num_workers):
-# 	# Uncomment this in case of QEFSGD_lossy
-#     # lrs.append({'lr':1e-3,'beta':0.9,'alpha':0.1})
-
-#     # rates alter for optimizer - it is documented in the report
-#     lrs.append({'lr':1e-3})
-
-# models = [Net() for i in range(num_workers)]
-
-# attacks = ['full_reversal','random_reversal', '' ]
-# protecs = ['median','trmean','majority',None ]
-
-# # Change the attack and protects from the array above
-# attack = ''
-# protec = None
-
-# # Byzantine nodes
-# # Add byzantine nodes to this array
-# # To run specific experiments add specific nodes - more information on this is available in the report
-# byzantine_nodes = []
-
-# net = Network(W, models, m, lrs, trainloaders, 32, nn.CrossEntropyLoss(), device, testloader, signSGD, [3,5], attack, protect,  )
-
-# iterations = 3000
-# epochs = 1
-# results = net.simulate(iterations, epochs)
-
-# train_accuracy = [ [ results[i][j]['train_acc'] for j in range(len(a[i])) ] for i in range(9) ]
-# test_concensus = [ [ results[i][j]['consensus_test'] for j in range(len(b[i])) ] for i in range(9) ]
-
-# iterations = [ [ results[i][j]['iteration'] for j in range(len(a[i])) ] for i in range(9) ]
-
-# # can alter this for various plots
-# plot_node = 0
-
-# plt.plot(iterations[plot_node],train_accuracy[plot_node],label="train_accuracy")
-# plt.plot(iterations[plot_node],test_concensus[plot_node],label="test_accuracy")
-# plt.legend()
-# plt.title("Consensus Train accuracy for Torus")
-# plt.ylabel("accuracy")
-# plt.xlabel("Iterations")
-# plt.savefig("Consensus_train_accuracy_Torus.jpg")
