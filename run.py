93e775aed999d2a366f913ce802e2bc947048d61
adityavarre <adityavarre232@gmail.com>
2020-06-12 23:10:54 +0200

    run.py for sample run

--- run.py
+++ run.py
@@ -31,26 +31,63 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=8,shuffle=False, nu
 
 classes = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
 
-
+#Loss function
 criterion = nn.CrossEntropyLoss()
-sqrt_num_workers = 3
+
+# Number of workers.
 num_workers = 9
+sqrt_num_workers = 3
 m = trainset_node_split(trainset, num_workers)
 trainloaders = [torch.utils.data.DataLoader(m[i], batch_size=32, shuffle=True, num_workers=2) for i in range(num_workers)]
-W = ring(num_workers)
+W = torus(sqrt_num_workers)
 lrs = []
 
+# EFSGD, signSGD, QSGD_lossy, QEFSGD_lossy, QSGD_topk, QEFSGD_topk
+# can choose optimizer from one of the above.
+optimizer = EFSGD
 
 for i in range(num_workers):
+	# Uncomment this in case of QEFSGD_lossy
     # lrs.append({'lr':1e-3,'beta':0.9,'alpha':0.1})
+
+    # rates alter for optimizer - it is documented in the report
     lrs.append({'lr':1e-3})
 
 models = [Net() for i in range(num_workers)]
 
-attacks = ['full_reversal','random_reversal']
-protecs = ['median','trmean','majority',None]
-net = Network(W, models, m, lrs, trainloaders, 32, nn.CrossEntropyLoss(), device, testloader, signSGD, [3,5], "full_reversal", None)
+attacks = ['full_reversal','random_reversal', '' ]
+protecs = ['median','trmean','majority',None ]
 
-a = net.simulate(20000, 1)
+# Change the attack and protects from the array above
+attack = ''
+protec = None
 
-pickle.dump(a,open("ring_signsgd_fullr_2node.pickle","wb"))
+# Byzantine nodes
+# Add byzantine nodes to this array
+# To run specific experiments add specific nodes - more information on this is available in the report
+byzantine_nodes = []
+
+net = Network(W, models, m, lrs, trainloaders, 32, nn.CrossEntropyLoss(), device, testloader, signSGD, [3,5], attack, protect,  )
+
+iterations = 3000
+epochs = 1
+results = net.simulate(iterations, epochs)
+
+train_accuracy = [ [ results[i][j]['train_acc'] for j in range(len(a[i])) ] for i in range(9) ]
+test_concensus = [ [ results[i][j]['consensus_test'] for j in range(len(b[i])) ] for i in range(9) ]
+
+iterations = [ [ results[i][j]['iteration'] for j in range(len(a[i])) ] for i in range(9) ]
+
+# can alter this for various plots
+plot_node = 0
+
+plt.plot(iterations[plot_node],train_accuracy[plot_node],label="train_accuracy")
+plt.plot(iterations[plot_node],test_concensus[plot_node],label="test_accuracy")
+plt.legend()
+plt.title("Consensus Train accuracy for Torus")
+plt.ylabel("accuracy")
+plt.xlabel("Iterations")
+plt.savefig("Consensus_train_accuracy_Torus.jpg")
+
+# Uncomment to dump a pickle file.
+# pickle.dump(a,open("ring_signsgd_fullr_2node.pickle","wb"))
